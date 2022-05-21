9aea56f9d166e7492da4470859268a9ee9369438
Syahrul Ramadhan <Sramadhan742@gmail.com>
2022-05-22 00:41:36 +0700

    update index dan layouts

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,171 @@
 <template>
-  <div>
+  <v-container fluid>
     <div v-if="!isSearchActive">
       <v-form ref="form" v-model="valid" lazy-validation>
         <v-row justify="center">
           <v-carousel cycle :show-arrows="false">
-          <v-carousel-item
-            v-for="(item,i) in items"
-            :key="i"
-            :src="item.src"
-          ></v-carousel-item>
-        </v-carousel>
+            <v-carousel-item v-for="(item, i) in items" :key="i">
+              <v-img contain :src="item.src"> </v-img>
+            </v-carousel-item>
+          </v-carousel>
         </v-row>
 
-        <v-row align="center" justify="center">
-          <v-col cols="6" sm="6" md="6">
-            <v-text-field
-              v-model="location"
-              :counter="25"
-              label="Cari lokasi kontes"
-              required
-            ></v-text-field>
+        <v-row align="center" justify="end" class="mx-2 my-2">
+          <v-col cols="4" sm="6" md="4" lg="4">
+            <v-card max-width="100%" class="px-3 py-3">
+              <v-row align="center">
+                <v-col cols="5">
+                  <v-text-field
+                    v-model="location"
+                    :counter="25"
+                    label="Cari lokasi kontes"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="5">
+                  <v-menu
+                    v-model="menu"
+                    :close-on-content-click="false"
+                    :nudge-right="40"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="auto"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-text-field
+                        v-model="date"
+                        label="Waktu kontes"
+                        prepend-icon="mdi-calendar"
+                        readonly
+                        v-bind="attrs"
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker
+                      v-model="date"
+                      @input="menu = false"
+                    ></v-date-picker>
+                  </v-menu>
+                </v-col>
+                <v-col cols="2" class="">
+                  <v-btn small color="primary" elevation="3" @click="search">
+                    Cari
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-card>
           </v-col>
-          <v-col cols="6" sm="6" md="6">
-            <v-menu
-              v-model="menu"
-              :close-on-content-click="false"
-              :nudge-right="40"
-              transition="scale-transition"
-              offset-y
-              min-width="auto"
-            >
-              <template v-slot:activator="{ on, attrs }">
-                <v-text-field
-                  v-model="date"
-                  label="Waktu kontes"
-                  prepend-icon="mdi-calendar"
-                  readonly
-                  v-bind="attrs"
-                  v-on="on"
-                ></v-text-field>
-              </template>
-              <v-date-picker
-                v-model="date"
-                @input="menu = false"
-              ></v-date-picker>
-            </v-menu>
-          </v-col>
-          <v-btn color="primary" class="mr-4" @click="search"> Cari </v-btn>
         </v-row>
+        <div class="my-3 py-3">
+          <v-row justify="center">
+            <h3>Informasi</h3>
+          </v-row>
+          <v-divider></v-divider>
+        </div>
+        <div class="my-3">
+          <v-row class="py-3">
+            <v-col cols="3" sm="12" md="3" lg="3">
+              <v-card class="mx-2" max-width="100%">
+                <v-img
+                  class="white--text align-end"
+                  height="200px"
+                  src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                >
+                  <v-card-title>Top 10 Australian beaches</v-card-title>
+                </v-img>
+
+                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+
+                <v-card-text class="text--primary">
+                  <div>Whitehaven Beach</div>
+
+                  <div>Whitsunday Island, Whitsunday Islands</div>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-btn color="orange" text> Share </v-btn>
+
+                  <v-btn color="orange" text> Explore </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+            <v-col cols="3" sm="12" md="3" lg="3">
+              <v-card class="mx-2" max-width="100%">
+                <v-img
+                  class="white--text align-end"
+                  height="200px"
+                  src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                >
+                  <v-card-title>Top 10 Australian beaches</v-card-title>
+                </v-img>
+
+                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+
+                <v-card-text class="text--primary">
+                  <div>Whitehaven Beach</div>
+
+                  <div>Whitsunday Island, Whitsunday Islands</div>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-btn color="orange" text> Share </v-btn>
+
+                  <v-btn color="orange" text> Explore </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+            <v-col cols="3" sm="12" md="3" lg="3">
+              <v-card class="mx-2" max-width="100%">
+                <v-img
+                  class="white--text align-end"
+                  height="200px"
+                  src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                >
+                  <v-card-title>Top 10 Australian beaches</v-card-title>
+                </v-img>
+
+                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+
+                <v-card-text class="text--primary">
+                  <div>Whitehaven Beach</div>
+
+                  <div>Whitsunday Island, Whitsunday Islands</div>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-btn color="orange" text> Share </v-btn>
+
+                  <v-btn color="orange" text> Explore </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+            <v-col cols="3" sm="12" md="3" lg="3">
+              <v-card class="mx-2" max-width="100%">
+                <v-img
+                  class="white--text align-end"
+                  height="200px"
+                  src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                >
+                  <v-card-title>Top 10 Australian beaches</v-card-title>
+                </v-img>
+
+                <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+
+                <v-card-text class="text--primary">
+                  <div>Whitehaven Beach</div>
+
+                  <div>Whitsunday Island, Whitsunday Islands</div>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-btn color="orange" text> Share </v-btn>
+
+                  <v-btn color="orange" text> Explore </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </div>
       </v-form>
     </div>
     <div v-else>
@@ -64,7 +182,7 @@
         />
       </v-row>
     </div>
-  </div>
+  </v-container>
 </template>
 
 <script>
@@ -102,7 +220,7 @@ export default {
     ],
     items: [
       {
-        src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+        src: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.avCzUDIgmLtWe1wb_MTdMgHaEK%26pid%3DApi&f=1",
       },
       {
         src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
