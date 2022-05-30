0215eb7f4766a174b27fd0cbebcfb0bc5ca29a88
Fandi02 <fandimaulana936@gmail.com>
2022-05-30 11:19:56 +0700

    feat: perbaikan

--- pages/testapi.vue
+++ pages/testapi.vue
@@ -10,7 +10,7 @@ export default {
   },
 
     async fetch() {
-      await this.$axios.get("/event").then((res) => (this.events = res));
+      await this.$axios.get("/event").then((res) => (this.events = res.data));
       console.log("hasil : "+ this.events);
     },
 }
